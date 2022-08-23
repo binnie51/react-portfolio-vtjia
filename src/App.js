7707681bfb8bd7e402b6d0a9f28a2f39db75cf81
Vincent Tjia <vtjia51@gmail.com>
2022-08-23 18:02:25 -0400

    Hero.js done. text not yet responsive

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 // import ContainerSite from "./components/ContainerSite";
 
 // Import page components
 import Navbar from './components/Navbar';
 import Footer from "./components/Footer";
 import Hero from "./components/pages/Hero";
+import About from "./components/pages/About";
+import Portfolio from "./components/pages/Portfolio";
+import Contact from './components/pages/Contact';
 // import { library } from '@fortawesome/fontawesome-svg-core';
 // import { fab } from '@fortawesome/free-brands-svg-icons';
 
@@ -12,11 +15,33 @@ import Hero from "./components/pages/Hero";
 
 // The whole app containing the app's body from Navbar to footer
 function App() {
+  const [currentPage, setCurrentPage] = useState('Hero');
+
+    // Method to check the value of `currentPage`
+    const renderPage = () => {
+        if (currentPage === 'Hero') {
+            return <Hero />;
+        }
+
+        if (currentPage === 'About') {
+            return <About />;
+        }
+
+        if (currentPage === 'Portfolio') {
+            return <Portfolio />;
+        }
+
+        if (currentPage === 'Contact') {
+            return <Contact />;
+        }
+    };
+
+    const handlePageChange = (page) => setCurrentPage(page);
   return (
     // <ContainerSite />
     <div>
-      <Navbar />
-      <Hero />
+      <Navbar currentPage={currentPage} handlePageChange={handlePageChange} />
+      {renderPage()}
       <Footer />
     </div>
   );
--- src/components/pages/Hero.js
+++ src/components/pages/Hero.js
@@ -1,12 +1,33 @@
 import React from "react";
 import '../styles/Hero.css';
 
+import resume from '../../assets/jr.web_VT.pdf';
+
 function Hero() {
     return (
         <div>
             <header className="container-fluid flex flex-wrap justify-center">
-                <div className="hero-image">
-
+                <div className="hero-image grid grid-rows-2">
+                    <div className="hero-text m-10 px-4 md:px-0">
+                        <div>
+                            <h1 className="font-black">
+                                Vincent Tjia
+                            </h1>
+                            <div  className="bg-slate-800" style={{height : 2 }}></div>
+                            <h2 className="font-bold">
+                                Web Developer, Designer
+                            </h2>
+                        </div>
+                        {/* resume */}
+                        <div className="mt-6 font-medium">
+                        <a className="myResumeBtn bg-amber-600 hover:bg-gray-900 hover:" 
+                        href={resume}
+                        target="_blank" 
+                        rel="noreferrer">
+                            View my resume
+                        </a>
+                        </div>
+                    </div>
                 </div>
 
             </header>
