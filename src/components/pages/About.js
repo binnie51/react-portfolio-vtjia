84b52c72e3cafc0b8c24433115ad1812d2f75694
Vincent Tjia <vtjia51@gmail.com>
2022-08-24 08:01:33 -0400

    about section done

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -2,11 +2,13 @@ import React from "react";
 import '../styles/About.css';
 
 import photo from '../../assets/profile-photo.png';
-import MySQL from '../../assets/mysql.png.jpg';
-import tailwind from '../../assets/tailwind.png';
-import express from '../../assets/express-js.png';
+// import MySQL from '../../assets/mysql.jpg';
+// import express from '../../assets/express-js.png';
+import { DiJavascript1, DiReact, DiJava } from 'react-icons/di';
+import { FaNode, FaBootstrap, FaGitSquare } from 'react-icons/fa';
+import { SiTailwindcss, SiMysql, SiPython, SiMongodb } from 'react-icons/si';
 
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 
 export default function About() {
     return (
@@ -16,7 +18,7 @@ export default function About() {
                 <div className="grid gap-10 md:grid-cols-2 pb-5">
                     <div className="overlay">
                         <div className="wrapper">
-                            <img src={photo} ></img>
+                            <img className="hover:bg-transparent" src={photo} ></img>
                         </div>
                     </div>
                     <div className="about-text text-white">
@@ -27,10 +29,37 @@ export default function About() {
                             I eventually took interest in programming, as I could utilize my multilingual and artistic background
                             to solve problems and produce creative end products.
                         </p>
-                        <p className="tracking-wide pt-4">Skills I have acquired:</p>
+                        <p className="tracking-wide pt-4 text-center">Skills I have acquired:</p>
                         <ul className="skills flex flex-wrap justify-center py-4 gap-7">
                             <li className="list-item">
-                                <i src={tailwind}></i>
+                                <DiJavascript1 />
+                            </li>
+                            <li className="list-item">
+                                <FaNode />
+                            </li>
+                            <li className="list-item">
+                                <SiTailwindcss />
+                            </li>
+                            <li className="list-item">
+                                <FaBootstrap />
+                            </li>
+                            <li className="list-item">
+                                <DiReact />
+                            </li>
+                            <li className="list-item">
+                                <SiMysql />
+                            </li>
+                            <li className="list-item">
+                                <SiPython />
+                            </li>
+                            <li className="list-item">
+                                <DiJava />
+                            </li>
+                            <li className="list-item">
+                                <FaGitSquare />
+                            </li>
+                            <li className="list-item">
+                                <SiMongodb />
                             </li>
                         </ul>
                     </div>
